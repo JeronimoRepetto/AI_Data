5b5af381d3be295e2b2a6b4bb773345d0d009523
Jeronimo Repetto <jeronimorepetto@gmail.com>
2023-04-20 20:47:54 +0200

    Update config_api.dart

--- ai_data/lib/core/app/http_service/config/config_api.dart
+++ ai_data/lib/core/app/http_service/config/config_api.dart
@@ -3,7 +3,7 @@ import 'package:ai_data/core/app/storage/local_storage.dart';
 
 class ConfigApi {
   ConfigApi();
-  final _gptApiKey = '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';
+  final _gptApiKey = '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';
   Future<Map<String, String>> headers() async {
     return {
       "Content-Type": "application/json",
